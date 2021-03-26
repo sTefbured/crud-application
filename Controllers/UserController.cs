e2496890bdc5fadee3eda21b02f1e3f29923cf96
sTefbured <stef.for.ever2000@gmail.com>
2021-03-27 02:38:13 +0300

    Add registration and login into controller.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,45 @@
-﻿namespace Lab1.Controllers
+﻿using Lab1.Models;
+using Lab1.Repository;
+using Lab1.Repository.Exception;
+
+namespace Lab1.Controllers
 {
-    public class AuthenticationController
+    public class UserController
     {
-        
+        private readonly IUserRepository _userRepository;
+        public User CurrentUser { get; private set; }
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+            CurrentUser = null;
+        }
+
+        public string Register(User user)
+        {
+            try
+            {
+                _userRepository.Add(user);
+                return "Registration successful.";
+            }
+            catch (UserExistsException exception)
+            {
+                return exception.Message;
+            }
+        }
+
+        public string Login(string login, string password)
+        {
+            try
+            {
+                var user = _userRepository.FindByLoginAndPassword(login, password);
+                CurrentUser = user;
+                return "Hello, " + login;
+            }
+            catch (UserNotFoundException exception)
+            {
+                return exception.Message;
+            }
+        }
     }
 }
